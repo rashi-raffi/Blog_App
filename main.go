afc39bbb78df0286a99c6d0ce3e56a4d1a0a00b4
rashi-raffi <rashi.m@converj.com>
2021-07-25 21:40:14 +0530

    new Handle func added for login.

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ func main() {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/api/user/new", controllers.CreateAccount).Methods("POST")
+	router.HandleFunc("/api/user/login", controllers.Authenticate).Methods("POST")
 	//router.Use(app.JwtAuthentication) //attach JWT auth middleware
 
 	//router.NotFoundHandler = app.NotFoundHandler
