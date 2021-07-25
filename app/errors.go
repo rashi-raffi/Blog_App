f4417b3a6515391fa9e7145ba7478bd8126f4b1e
rashi-raffi <rashi.m@converj.com>
2021-07-26 02:28:06 +0530

    Code updated.

--- app/errors.go
+++ app/errors.go
@@ -10,6 +10,6 @@ var NotFoundHandler = func(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusNotFound)
 		u.Respond(w, u.Message(false, "This resources was not found on our server"))
-		next.ServeHTTP(w, r)
+		//next.ServeHTTP(w, r)
 	})
 }
