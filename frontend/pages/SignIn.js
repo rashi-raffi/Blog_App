1f50d4c389115b5ccdbfd60e664d7328cd7620c4
rashi-raffi <rashi.m@converj.com>
2021-08-09 10:19:17 +0530

    Sign in page added.

---
+++ frontend/pages/SignIn.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react'
+import axios from 'axios';
+import router from 'next/router';
+import Link from 'next/link'
+import cookieCutter from 'cookie-cutter'
+
+class SignIn extends Component {
+    state = { 
+        userName: '',
+        email: '',
+        password: '',
+        token: ''
+    }
+
+    isTokenValid = () => {
+      const token = localStorage.getItem("token")
+      if(token){
+        console.log(token)
+        router.push('/blogs')
+      }
+    }
+
+    onFormSubmit = async (event) => {
+        event.preventDefault();
+        try {
+          const response = await axios.post("http://localhost:8000/api/user/login  ", {
+            email: this.state.email,
+            password: this.state.password
+          });
+          if (response.data.status) {
+            localStorage.setItem("token", response.data.token)
+            router.push('/SignIn')
+          }
+        } catch (error) {
+          console.log(error);
+        };
+    }
+
+    async componentDidMount() {
+      this.isTokenValid()
+    }
+    
+
+
+    render(){
+        return(
+          <div>
+              <p>Sign In </p>
+              <form onSubmit={this.onFormSubmit}>
+                  <input type="text" placeholder="Email id" value={this.state.email} onChange={(e) => this.setState({ email: e.target.value })}  /><br />
+                  <input type="password" placeholder="Password" value={this.state.password} onChange={(e) => this.setState({ password: e.target.value })}  /><br />
+                  <input type="submit" value="Submit" />
+                  <div >
+                    <p>Already Registered?<br/><Link href="/SignIn"><a>Signin</a></Link></p>
+                  </div>
+              </form>
+          </div>
+        );
+      };
+}
+
+export default SignIn;
