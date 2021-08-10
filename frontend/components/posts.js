d062ef8d4ddc100d3b6cf9966193dfc8725d6b82
rashi-raffi <rashi.m@converj.com>
2021-08-10 12:32:44 +0530

    Post component added

--- frontend/components/posts.js
+++ frontend/components/posts.js
@@ -4,29 +4,13 @@ class BlogPost extends Component {
     state = {
         blogs: [],
     };
-
-    // componentDidMount() {
-    //     this.getBlog();
-    // }
-
     render() {
         const BlogsList = this.props.blogs.data;
         if (BlogsList) {
             console.log(Object.values(BlogsList).length);
-            // if(Object.values(BlogsList).length > 0){
-            //     return (<div>
-            //         {Object.values(BlogsList).map((bgs, index) => (
-            //             <div key={index}>
-            //                 <div> {bgs.title} </div>
-            //                 <div> {bgs.text} </div>
-            //             </div>
-
-            //         ))}
-            //         </div>);
-            // }
             if (Object.values(BlogsList).length > 0) {
                 return (
-                    <div className="container" style={{ margin: "50px" }}>
+                    <div className="container" style={{ marginTop: "50px" }}>
                         {Object.values(BlogsList).map((bgs, index) => (
                             <div
                                 className="ui card"
