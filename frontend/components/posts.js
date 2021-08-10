d12984b1443266f277f3e0cb63dfc0fe9e4ac5b0
rashi-raffi <rashi.m@converj.com>
2021-08-10 11:32:33 +0530

    Styling added to the blogs.

--- frontend/components/posts.js
+++ frontend/components/posts.js
@@ -1,35 +1,61 @@
-import React, { Component } from 'react'
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 class BlogPost extends Component {
     state = {
-        blogs: []
-    }
-    
+        blogs: [],
+    };
+
     // componentDidMount() {
     //     this.getBlog();
     // }
-    
-    render() { 
-        const BlogsList = this.props.blogs.data
-        if(BlogsList){
-        console.log(Object.values(BlogsList).length)
-        if(Object.values(BlogsList).length > 0){
-            return (<div>
-                {Object.values(BlogsList).map((bgs, index) => (
-                    <div key={index}>
-                        <div> {bgs.title} </div>
-                        <div> {bgs.text} </div>
+
+    render() {
+        const BlogsList = this.props.blogs.data;
+        if (BlogsList) {
+            console.log(Object.values(BlogsList).length);
+            // if(Object.values(BlogsList).length > 0){
+            //     return (<div>
+            //         {Object.values(BlogsList).map((bgs, index) => (
+            //             <div key={index}>
+            //                 <div> {bgs.title} </div>
+            //                 <div> {bgs.text} </div>
+            //             </div>
+
+            //         ))}
+            //         </div>);
+            // }
+            if (Object.values(BlogsList).length > 0) {
+                return (
+                    <div className="container" style={{ margin: "50px" }}>
+                        {Object.values(BlogsList).map((bgs, index) => (
+                            <div
+                                className="ui card"
+                                key={index}
+                                style={{ width: "auto" }}
+                            >
+                                <div className="content">
+                                    <div className="header">{bgs.title}</div>
+                                    <div className="meta">
+                                        <span>{Date(bgs.CreatedAt)}</span>
+                                    </div>
+                                    <p>{bgs.text}</p>
+                                </div>
+                            </div>
+                        ))}
                     </div>
-                    
-                ))}
-                </div>);
+                );
+            }
         }
-        }
-        return(
-            <div>
-                No post found..!
-            </div>)
+        return (
+            <div className="container" style={{ margin: "50px" }}>
+                <div className="ui card" style={{ width: "auto" }}>
+                    <div className="content">
+                        <div className="header">No Posts found...!</div>
+                    </div>
+                </div>
+            </div>
+        );
     }
 }
- 
+
 export default BlogPost;
