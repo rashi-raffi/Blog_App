20576769e0033a7504a1e37069beb1925114d981
rashi-raffi <rashi.m@converj.com>
2021-08-10 14:22:29 +0530

    Code formatted.

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,20 +1,20 @@
-import '../styles/globals.css'
-import Head from 'next/head';
+import "../styles/globals.css";
+import Head from "next/head";
 function MyApp({ Component, pageProps }) {
-  // return (
-  //   <Component {...pageProps} />
-  // )
-  return (
-    <>
-    <Head>
-      <link
-            href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css"
-            rel="stylesheet"
-      />
-    </Head>
-    <Component {...pageProps} />
-    </>
-    )
+    // return (
+    //   <Component {...pageProps} />
+    // )
+    return (
+        <>
+            <Head>
+                <link
+                    href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css"
+                    rel="stylesheet"
+                />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 }
 
-export default MyApp
+export default MyApp;
