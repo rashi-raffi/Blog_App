b278b5db2f67b6565a8294c5fb8bb7c0d57700de
rashi-raffi <rashi.m@converj.com>
2021-08-10 13:02:08 +0530

    Style added to SignIn page.

--- frontend/pages/SignIn.js
+++ frontend/pages/SignIn.js
@@ -43,39 +43,110 @@ class SignIn extends Component {
         this.isTokenValid();
     }
 
+    // render() {
+    //     return (
+    //         <div>
+    //             <p>Sign In </p>
+    //             <form onSubmit={this.onFormSubmit}>
+    //                 <input
+    //                     type="text"
+    //                     placeholder="Email id"
+    //                     value={this.state.email}
+    //                     onChange={(e) =>
+    //                         this.setState({ email: e.target.value })
+    //                     }
+    //                 />
+    //                 <br />
+    //                 <input
+    //                     type="password"
+    //                     placeholder="Password"
+    //                     value={this.state.password}
+    //                     onChange={(e) =>
+    //                         this.setState({ password: e.target.value })
+    //                     }
+    //                 />
+    //                 <br />
+    //                 <input type="submit" value="Submit" />
+    //                 <div>
+    //                     <p>
+    //                         Not Already ?<br />
+    //                         <Link href="/SignUp">
+    //                             <a>Signin</a>
+    //                         </Link>
+    //                     </p>
+    //                 </div>
+    //             </form>
+    //         </div>
+    //     );
+    // }
+
     render() {
         return (
-            <div>
-                <p>Sign In </p>
-                <form onSubmit={this.onFormSubmit}>
-                    <input
-                        type="text"
-                        placeholder="Email id"
-                        value={this.state.email}
-                        onChange={(e) =>
-                            this.setState({ email: e.target.value })
-                        }
-                    />
-                    <br />
-                    <input
-                        type="password"
-                        placeholder="Password"
-                        value={this.state.password}
-                        onChange={(e) =>
-                            this.setState({ password: e.target.value })
-                        }
-                    />
-                    <br />
-                    <input type="submit" value="Submit" />
-                    <div>
-                        <p>
-                            Not Already ?<br />
-                            <Link href="/SignUp">
-                                <a>Signin</a>
+            <div
+                className="ui container"
+                style={{
+                    margin: "auto",
+                    marginTop: "10%",
+                    width: "60%",
+                    padding: "10px",
+                }}
+            >
+                <div className="ui placeholder segment">
+                    <div className="ui two column very relaxed stackable grid">
+                        <div className="column">
+                            <form
+                                onSubmit={this.onFormSubmit}
+                                className="ui form"
+                            >
+                                <div className="field">
+                                    <label>Username</label>
+                                    <div className="ui left icon input">
+                                        <input
+                                            type="text"
+                                            placeholder="Username"
+                                            value={this.state.email}
+                                            onChange={(e) =>
+                                                this.setState({
+                                                    email: e.target.value,
+                                                })
+                                            }
+                                        />
+                                        <i className="user icon"></i>
+                                    </div>
+                                </div>
+                                <div className="field">
+                                    <label>Password</label>
+                                    <div className="ui left icon input">
+                                        <input
+                                            type="password"
+                                            value={this.state.password}
+                                            onChange={(e) =>
+                                                this.setState({
+                                                    password: e.target.value,
+                                                })
+                                            }
+                                        />
+                                        <i className="lock icon"></i>
+                                    </div>
+                                </div>
+                                <input
+                                    className="ui blue submit button"
+                                    type="submit"
+                                    value="Login In"
+                                />
+                            </form>
+                        </div>
+                        <div className="middle aligned column">
+                            <Link href="/signUp">
+                                <div className="ui big button">
+                                    <i className="signup icon"></i>
+                                    Sign Up
+                                </div>
                             </Link>
-                        </p>
+                        </div>
                     </div>
-                </form>
+                    <div className="ui vertical divider">Or</div>
+                </div>
             </div>
         );
     }
